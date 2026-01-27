a4c4469f30307e06d9ee32507b37623da3407ae2
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-01-27 12:12:52 +0530

    test.js added

---
+++ JavaScript/test.js
@@ -0,0 +1,31 @@
+var v = 2
+console.log(typeof v)
+
+const a = Symbol("id")
+console.log(typeof a)
+
+let b = null
+console.log(typeof b)
+
+c = "hello"
+console.log(typeof c)
+
+d = 42n
+console.log(typeof d)
+
+e = true
+console.log(typeof e)
+
+f = {}
+console.log(typeof f)
+
+g = function() {}
+console.log(typeof g)
+
+h = []
+console.log(typeof h)
+
+console.log(typeof h[0])
+
+i = undefined
+console.log(typeof i)
