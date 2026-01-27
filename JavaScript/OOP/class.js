dbcae6bba849a767a0af5e9f1155e2f7b71335a5
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-01-27 12:22:06 +0530

    OOP added in JS

---
+++ JavaScript/OOP/class.js
@@ -0,0 +1,37 @@
+var person = {
+    name: "Nahi Bataunga",
+    age: 21,
+    roll: 30,
+    displayInfo: function displayInfo() {
+        console.log("Name: " + this.name + ", Age: " + this.age + ", Roll: " + this.roll);
+    }
+}
+console.log(person.name);  // Accessing property
+console.log(person["name"]);  // Accessing property using bracket notation
+person.displayInfo();
+
+// Using Constructor Function
+function Person(name, age, roll) {
+    this.name = name;
+    this.age = age;
+    this.roll = roll;
+    this.displayInfo = function() {
+        console.log("Name: " + this.name + ", Age: " + this.age + ", Roll: " + this.roll);
+    }
+}  
+var person1 = new Person("Nahi Bataunga", 21, 30);
+person1.displayInfo();
+
+// Using ES6 Class
+class PersonClass {
+    constructor(name, age, roll) {
+        this.name = name;
+        this.age = age;
+        this.roll = roll;
+    }
+    displayInfo() {
+        console.log("Name: " + this.name + ", Age: " + this.age + ", Roll: " + this.roll);
+    }
+}
+var person2 = new PersonClass("Nahi Bataunga", 21, 30);
+person2.displayInfo();
