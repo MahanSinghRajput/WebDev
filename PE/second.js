33ae4673795db60216a4c25b7db000b027e93fb5
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-01-30 14:06:48 +0530

    added in second.js

--- PE/second.js
+++ PE/second.js
@@ -7,5 +7,28 @@ console.log(obj.name);
 
 function greet(name){
     console.log("Hello, " + name);
-    console.log(`Hello, ${name}!`); //use this syntax
+    console.log(`Hello, ${name}, how are you?`); //use this syntax
 }
+greet("Dennis");
+
+//More about functions
+function square(x) {
+    return x * x;
+}
+console.log(square(4));
+
+const factorial  = function(n){ //anonymous function
+    if(n==0 || n==1) return 1;
+    return n * factorial(n-1);
+}
+console.log(factorial(5));
+
+const arrowFunc = (x) => x + 10;
+console.log(arrowFunc(5));
+
+//IIFE - Immediately Invoked Function Expression
+//IIFE means a function that runs as soon as it is defined
+(function() {
+    console.log("This function runs immediately upon definition!");
+})();
+//The code above demonstrates various aspects of functions in JavaScript. It includes a standard function declaration (`greet`), a function expression assigned to a constant (`factorial`), an arrow function (`arrowFunc`), and an Immediately Invoked Function Expression (IIFE). Each function showcases different ways to define and use functions in JavaScript.
