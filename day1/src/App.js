f43ef143016e4a88c31853b06f901b0485bfe56b
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-02-02 14:52:14 +0530

    added notes

--- day1/src/App.js
+++ day1/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';  //packages //shows directory structure './' shows same package
 
+// (*) you can also write it as export default function App(){}
 function App() {
   return (
     //always use div to store these values as without div these would be treated as seperate elements and we cant return two elements in a function.
@@ -12,4 +13,5 @@ function App() {
   );
 }
 
+//if you do the (*) step then you dont need this statement
 export default App;
