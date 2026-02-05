31c0caa60bdd9fc573a20695c9063a11ddfa4361
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-02-05 16:38:28 +0530

    header fixed

--- day2/src/components/Header.jsx
+++ day2/src/components/Header.jsx
@@ -1,5 +1,11 @@
 export default function Header(){
     return(
+        <>
         <h1> This is my first app</h1>
+        <nav>
+            <a href = "#">Home</a>
+            <a href = "#">Contact Us</a>
+        </nav>
+        </>
     )
 }
