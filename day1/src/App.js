4922dc7f074a9d870b33d21e7ac92a02965bbbf9
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-02-05 15:46:59 +0530

    pata nahi kyu nahi chal rha

--- day1/src/App.js
+++ day1/src/App.js
@@ -4,10 +4,10 @@ import './App.css';  //packages //shows directory structure './' shows same pack
 function App() {
   return (
     //always use div to store these values as without div these would be treated as seperate elements and we cant return two elements in a function.
-    <div>
+    <>
     <h1> Hello </h1>
     <h2> bhai</h2>
-    </div>
+    </>
 
     //whatever you want to show on the browser is written ion return() else things are written outside
   );
