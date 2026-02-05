9fa2d49ca6fee1701f9885f9932059c1a0f00427
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-02-05 16:43:39 +0530

    css added for header

--- day2/src/components/Header.jsx
+++ day2/src/components/Header.jsx
@@ -1,8 +1,9 @@
+import './Header.css'
 export default function Header(){
     return(
         <>
         <h1> This is my first app</h1>
-        <nav>
+        <nav className = "navigation">
             <a href = "#">Home</a>
             <a href = "#">Contact Us</a>
         </nav>
