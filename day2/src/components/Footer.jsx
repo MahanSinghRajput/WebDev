494f06bd99e682ececdc7c96d5c26a7f0c086875
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-02-06 14:17:52 +0530

    footer added

--- day2/src/components/Footer.jsx
+++ day2/src/components/Footer.jsx
@@ -1,7 +1,10 @@
+import './Footer.css'
 export default function Footer(){
     return(
+        <div className='foot'>
         <footer>
             <p>2026 by ME</p>
         </footer>
+        </div>
     )
 }
