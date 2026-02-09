1c8851425f04fdddb81420365262dbb3549bbc21
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-02-09 12:05:06 +0530

    added 2d function

--- JavaScript/array.js
+++ JavaScript/array.js
@@ -1,2 +1,5 @@
 //array doesn't need to have same data type
 let arr = [1, 'hello', true, null, undefined, {name: 'John'}, [1, 2, 3]];
+
+let arr2 = [[1,2],[3,5]]
+for([i,j] of arr2) console.log(i,j)
