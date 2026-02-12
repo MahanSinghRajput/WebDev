b6d8d001a941e63f20ddac7f348429f203a0f383
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-02-12 16:45:04 +0530

    to be solved

--- DemoApp/src/components/EventToDo.jsx
+++ DemoApp/src/components/EventToDo.jsx
@@ -1,17 +1,52 @@
+// export default function EventToDo(){
+//     //when you call this handler in an event then dont put round brackets, then only it will work when called by the event.
+//     function handleClick(prop){
+//         alert(`${prop} just pressed the button.`)
+//     }
+//     function handleDoubleClick(){
+//         alert(`bas kar bhai`)
+//     }
+//     return(
+//         <div>
+//             <h1>Event Heading</h1>
+//             <button onClick = {() => handleClick("ABC")}>Click Me</button>
+//             <br></br>
+//             <button onDoubleClick={() => handleDoubleClick()}>Change Background</button>
+//         </div>
+//     )
+// }
+
 export default function EventToDo(){
-    //when you call this handler in an event then dont put round brackets, then only it will work when called by the event.
-    function handleClick(prop){
-        alert(`${prop} just pressed the button.`)
+    function handleSubmit(e){
+        e.preventDefault() //to prevent reload on occouring of an event
+        const input = e.target.value
+        const taskValue = input.value.trim()
+        if(taskValue === "") {
+            alert("Empty task cannot be added")
+            return
+        }
+        const li = document.createElement('li')
+        li.innerHTML = `<span>${taskValue}</span>
+                        <button class = "deleteButton">Delete</button>`
+        
+        document.getElementById("list").appendChild(li)
     }
-    function handleDoubleClick(){
-        alert(`bas kar bhai`)
+    function handleListClick(e){
+        if(e.target.className === "deleteButton"){
+            e.target.parentElement.remove()
+        }
+
     }
     return(
-        <div>
-            <h1>Event Heading</h1>
-            <button onClick = {() => handleClick("ABC")}>Click Me</button>
-            <br></br>
-            <button onDoubleClick={() => handleDoubleClick()}>Change Background</button>
-        </div>
+        <>
+        <h1>To Do List</h1>
+        <form onSubmit={() => handleSubmit()}>
+            <input type = "text" name = "task" placeholder="Enter Task">New</input>
+            <button type = "submit">Add Task</button>
+        </form>
+        <ul id = "list" onClick={handleListClick}>
+
+        </ul>
+        </>
     )
 }
