c788479138f6fba1caf52f248274e6767a88b622
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-02-13 15:15:05 +0530

    changes to be made

--- DemoApp/src/components/EventToDo.jsx
+++ DemoApp/src/components/EventToDo.jsx
@@ -16,6 +16,41 @@
 //     )
 // }
 
+// export default function EventToDo(){
+//     function handleSubmit(e){
+//         e.preventDefault() //to prevent reload on occouring of an event
+//         const input = e.target.task.value
+//         const taskValue = input.trim()
+//         if(taskValue === "") {
+//             alert("Empty task cannot be added")
+//             return
+//         }
+//         const li = document.createElement('li')
+//         li.innerHTML = `<span>${taskValue}</span>
+//                         <button class = "deleteButton">Delete</button>`
+        
+//         document.getElementById("list").appendChild(li)
+//     }
+//     function handleListClick(e){
+//         if(e.target.className === "deleteButton"){
+//             e.target.parentElement.remove()
+//         }
+
+//     }
+//     return(
+//         <>
+//         <h1>To Do List</h1>
+//         <form onSubmit={handleSubmit}>
+//             <input type = "text" name = "task" placeholder="Enter Task" />
+//             <button type = "submit">Add Task</button>
+//         </form>
+//         <ul id = "list" onClick={handleListClick}>
+            
+//         </ul>
+//         </>
+//     )
+// }
+
 export default function EventToDo(){
     function handleSubmit(e){
         e.preventDefault() //to prevent reload on occouring of an event
@@ -41,8 +76,9 @@ export default function EventToDo(){
         <>
         <h1>To Do List</h1>
         <form onSubmit={handleSubmit}>
-            <input type = "text" name = "task" placeholder="Enter Task" />
-            <button type = "submit">Add Task</button>
+            <input type = "text" placeholder="Enter Task" />
+            value = {task}
+            onChange = {(e)=>setTask(e.target.value)}
         </form>
         <ul id = "list" onClick={handleListClick}>
             
