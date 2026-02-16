1f4dc48a211121a9fb79240ed6919616643d0bb9
MahanSinghRajput <mahan.2428cse1312@kiet.edu>
2026-02-16 12:16:46 +0530

    async driver added

--- JavaScript/syncAsync.js
+++ JavaScript/syncAsync.js
@@ -1,4 +1,15 @@
 async function greet(){
     console.log("hello")
 }
-greet().then(console.log)
+//greet().then(console.log)
+
+const testAsync = async function() {
+    setTimeout(()=>{
+        console.log("async")
+    },3000)
+}
+async function driver(){
+    await testAsync()
+    console.log("execution completed")
+}
+driver()
